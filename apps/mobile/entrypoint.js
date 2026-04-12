3d4dd2db258134e95c46fbfac06bd8c58d8d3206
jikalle <shehuusman1414@gmail.com>
2026-04-12 13:17:08 +0100

    Polyfill Event globals for mobile startup

--- apps/mobile/entrypoint.js
+++ apps/mobile/entrypoint.js
@@ -1,3 +1,39 @@
+// Minimal browser event polyfills required by wallet/auth dependencies.
+if (typeof globalThis.Event !== 'function') {
+  class EventPolyfill {
+    constructor(type, options = {}) {
+      this.type = type
+      this.bubbles = Boolean(options.bubbles)
+      this.cancelable = Boolean(options.cancelable)
+      this.composed = Boolean(options.composed)
+      this.defaultPrevented = false
+      this.timeStamp = Date.now()
+    }
+
+    preventDefault() {
+      if (this.cancelable) {
+        this.defaultPrevented = true
+      }
+    }
+
+    stopPropagation() {}
+    stopImmediatePropagation() {}
+  }
+
+  globalThis.Event = EventPolyfill
+}
+
+if (typeof globalThis.CustomEvent !== 'function') {
+  class CustomEventPolyfill extends globalThis.Event {
+    constructor(type, options = {}) {
+      super(type, options)
+      this.detail = options.detail ?? null
+    }
+  }
+
+  globalThis.CustomEvent = CustomEventPolyfill
+}
+
 // Import required polyfills first
 import 'fast-text-encoding'
 import 'react-native-get-random-values'
